ca598abeae17e4f6a33f09b8ef2691880b396fb6
Ben Wendt <ben@theworkinggroup.ca>
2017-12-07 14:32:32 -0500

    Updates after help from Aaron

--- day3b.rs
+++ day3b.rs
@@ -1,21 +1,23 @@
 
 const WIDTH: usize = 1000;
 const HEIGHT: usize = 1000;
-const UP: u8 = 0;
-const LEFT: u8 = 1;
-const RIGHT: u8 = 2;
-const DOWN: u8 = 3;
+
+enum Direction {
+    Up, Left, Right, Down
+}
+
+fn get_memory() -> [[usize; WIDTH]; HEIGHT] {
+  [[0; WIDTH]; HEIGHT]
+}
 
 fn main() {
 
-    let mut memory_raw = vec![0; WIDTH * HEIGHT];
-    let mut memory_base: Vec<_> = memory_raw.as_mut_slice().chunks_mut(WIDTH).collect();
-    let memory: &mut [&mut [_]] = memory_base.as_mut_slice();
-    
+    let mut memory = get_memory();
+
     let val = 312051;
     let mut locx = WIDTH / 2;
     let mut locy = HEIGHT / 2;
-    let mut dir = UP;
+    let mut dir = Direction::Up;
     let mut highest = 0;
 
     memory[locx][locy] = 1;
@@ -24,14 +26,11 @@ fn main() {
             break;
         }
 
-        if dir == RIGHT {
-            locx = locx + 1;
-        } else if dir == LEFT {
-            locx = locx - 1;
-        } else if dir == UP {
-            locy = locy - 1;
-        } else if dir == DOWN {
-            locy = locy + 1;
+        match dir {
+         Direction::Right => locx = locx + 1,
+         Direction::Left  => locx = locx - 1,
+         Direction::Up    => locy = locy - 1,
+         Direction::Down  => locy = locy + 1
         }
 
         memory[locx][locy] = memory[locx - 1][locy - 1] +
@@ -45,17 +44,17 @@ fn main() {
         
         if memory[locx][locy] > highest {
             highest = memory[locx][locy];
-            println!("New highest at {} {} is {}", locx, locy, highest)
+            // println!("New highest at {} {} is {}", locx, locy, highest)
         }
 
         if memory[locx][locy + 1] == 0 && memory[locx + 1][locy] != 0 {
-            dir = DOWN;
+            dir = Direction::Down;
         } else if memory[locx + 1][locy] == 0 && memory[locx][locy - 1] != 0 {
-            dir = RIGHT;
+            dir = Direction::Right;
         } else if memory[locx][locy -1] == 0 && memory[locx -1][locy] != 0{
-            dir = UP;
+            dir = Direction::Up;
         } else if memory[locx - 1][locy] == 0 {
-            dir = LEFT;
+            dir = Direction::Left;
         }
     }
     println!("Highest value is {}", highest);
