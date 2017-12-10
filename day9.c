54de0d253f80af0f1fb5ac78fb89a18a54d99688
Ben Wendt <ben@twg.io>
2017-12-09 22:48:44 -0500

    factor out functions

--- day9.c
+++ day9.c
@@ -5,62 +5,91 @@
 
 enum State {Adding = 0, Garbage = 1, Deleting = 2};
 
+struct Result {
+    int sum;
+    int depth;
+    int garbage;
+    enum State state;
+    enum State previousState;
+};
+
+struct Result handleAdding(struct Result result, char c) {
+    switch(c) {
+        case '{':
+            result.depth++;
+            break;
+        case '}':
+            result.sum += result.depth;
+            result.depth--;
+            break;
+        case ',':
+            break;
+        case '<':
+            result.state = Garbage;
+            break;
+    }
+
+    return result;
+}
+
+struct Result handleGarbage(struct Result result, char c) {
+    switch(c) {
+        case '!':
+            result.previousState = Garbage;
+            result.state = Deleting;
+            break;
+        case '>':
+            result.state = Adding;
+            break;
+        default:
+            result.garbage++;
+            break;
+    }
+
+    return result;
+}
+
+struct Result handleDeleting(struct Result result, char c) {
+    switch(c) {
+        default:
+            result.state = result.previousState;
+            break;
+    }
+    return result;
+}
+
+struct Result figureOut(char buff[BUFF_SIZE]) {
+    
+    struct Result result = {0, 0, 0, Adding, Adding};
+
+    for (int i = 0; i < strlen(buff); i++) {
+        switch(result.state) {
+            case Adding:
+                result = handleAdding(result, buff[i]);
+                break;
+            case Garbage:
+                result = handleGarbage(result, buff[i]);
+                break;
+            case Deleting:
+                result = handleDeleting(result, buff[i]);
+                break;
+        }
+    }
+    
+    return result;
+}
+
 int main(int argc, char **argv)
 {
     char buff[BUFF_SIZE];
     FILE *f = fopen("data/day9.txt", "r");
     fgets(buff, BUFF_SIZE, f);
-    int currentSum = 0;
-    int currentDepth = 0;
-    int garbageCharacters = 0;
-
-    enum State state = Adding;
-    enum State previousState = Adding;
-    
-
-    for (int i = 0; i < strlen(buff); i++) {
-        switch(state) {
-            case Adding:
-                switch(buff[i]) {
-                    case '{':
-                        currentDepth++;
-                        break;
-                    case '}':
-                        currentSum += currentDepth;
-                        currentDepth--;
-                        break;
-                    case ',':
-                        break;
-                    case '<':
-                        state = Garbage;
-                        break;
-                }
-                break;
-            case Garbage:
-                switch(buff[i]) {
-                    case '!':
-                        previousState = Garbage;
-                        state = Deleting;
-                        break;
-                    case '>':
-                        state = Adding;
-                        break;
-                    default:
-                        garbageCharacters++;
-                        break;
-                }
-                break;
-            case Deleting:
-                switch(buff[i]) {
-                    default:
-                        state = previousState;
-                        break;
-                }
-                break;
-        }
-    }
-    printf("Sum: %d, final depth: %d, garbage characters: %d\n", currentSum, currentDepth, garbageCharacters);
     fclose(f);
+
+    struct Result result = figureOut(buff);
+    printf("Sum: %d\n", result.sum);
+    printf("final depth: %d\n", result.depth);
+    printf("garbage characters: %d\n", result.garbage);
     
     return 0;
 }
